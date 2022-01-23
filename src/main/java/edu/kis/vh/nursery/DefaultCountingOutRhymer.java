ab9b7617a09dee2ad4d7b0fd1f3f391b3b8c4f02
Jonasz Potoniec <jonasz.potoniec@gmail.com>
2022-01-23 16:31:55 +0100

    Added requested changes

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,12 +6,12 @@ public class DefaultCountingOutRhymer {
     public static final int NUMBER_OF_NUMBERS = 12;
     public static final int RETURN_ERROR_VALUE = -1;
 
-    private final int[] numbers = new int[NUMBER_OF_NUMBERS];
+    private final int[] NUMBERS = new int[NUMBER_OF_NUMBERS];
     public int total = INITIAL_TOTAL;
 
     public void countIn(int in) {
         if (!isFull())
-            numbers[++total] = in;
+            NUMBERS[++total] = in;
     }
 
     public boolean callCheck() {
@@ -25,13 +25,13 @@ public class DefaultCountingOutRhymer {
     protected int peekaboo() {
         if (callCheck())
             return RETURN_ERROR_VALUE;
-        return numbers[total];
+        return NUMBERS[total];
     }
 
     public int countOut() {
         if (callCheck())
             return RETURN_ERROR_VALUE;
-        return numbers[total--];
+        return NUMBERS[total--];
     }
 
 }
